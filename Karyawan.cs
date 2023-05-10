758a1793835ebe68e0d019ce4b53365cd59387fa
Illyaz Arya <71999096+McIllyaz@users.noreply.github.com>
2023-05-10 20:54:10 +0700

    Add files via upload

---
+++ Karyawan.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ResponsiPemrograman5156
+{
+	class Karyawan
+	{
+		public int NIK { get; set; }
+		public string Nama { get; set; }
+		private int gajiBulanan;
+
+
+		public int GajiBulanan
+		{
+            get { return gajiBulanan; }
+            set { gajiBulanan = value < 0 ? 0 : value; }
+        }
+
+
+		public void Print()
+		{
+			Console.WriteLine("NIK\t: {0}", NIK);
+			Console.WriteLine("Nama\t: {0}", Nama);
+			Console.WriteLine("Gaji\t: {0}", GajiBulanan);
+		}
+
+		public void naikgaji()
+		{
+			GajiBulanan += (int)(GajiBulanan * 0.1);
+            Console.WriteLine("+ 10%\t: {0}", GajiBulanan);
+        }
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ResponsiPemrograman5156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan_1 = new Karyawan();
+            Karyawan karyawan_2 = new Karyawan();
+            Karyawan karyawan_3 = new Karyawan();
+            Karyawan karyawan_4 = new Karyawan();
+            Karyawan karyawan_5 = new Karyawan();
+
+
+            karyawan_1.NIK = 6471;
+            karyawan_1.Nama = "Arya";
+            karyawan_1.GajiBulanan = 50000000;
+
+            karyawan_1.Print();
+            karyawan_1.naikgaji();
+            Console.WriteLine();
+
+
+            karyawan_2.NIK = 6472;
+            karyawan_2.Nama = "Ando";
+            karyawan_2.GajiBulanan = 5000000;
+
+            karyawan_2.Print();
+            karyawan_2.naikgaji();
+            Console.WriteLine();
+
+
+            karyawan_3.NIK = 6473;
+            karyawan_3.Nama = "Ardi";
+            karyawan_3.GajiBulanan = 4000000;
+
+            karyawan_3.Print();
+            karyawan_3.naikgaji();
+            Console.WriteLine();
+
+
+            karyawan_4.NIK = 6474;
+            karyawan_4.Nama = "Ardo";
+            karyawan_4.GajiBulanan = 50000000;
+
+            karyawan_4.Print();
+            karyawan_4.naikgaji();
+            Console.WriteLine();
+
+
+            karyawan_5.NIK = 6475;
+            karyawan_5.Nama = "Andi";
+            karyawan_5.GajiBulanan = 4500000;
+
+            karyawan_5.Print();
+            karyawan_5.naikgaji();
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
